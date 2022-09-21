2f058792a644f24d14447beee2f5c30b95f044bf
fengkun <fengkun@longshine.com>
2022-09-21 17:54:15 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <view class="index">
+    <Counter />
+    <div>{{ count }}</div>
+    <div>ojbk.age{{ ojbk.age }}</div>
+    <button @click="add">add</button>
+  </view>
+</template>
+
+<script setup>
+import { ref, reactive, effect } from "vue";
+import "./index.less";
+import Counter from "../../components/Counter.vue";
+
+const ojbk = reactive({
+  age: 10
+});
+
+const count = ref(1);
+
+effect(() => {
+  ojbk.age = ojbk.age + count.value;
+  console.log(ojbk.age, "init");
+});
+
+const add = () => {
+  count.value++;
+};
+</script>
